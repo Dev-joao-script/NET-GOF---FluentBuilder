2bade9db5bc14283b7a4b0475224ba6aea65ca92
Dev-joao-script <Dev.joao.script@gmail.com>
2022-10-28 18:05:28 -0200

    Fundamentos .NET GOF

---
+++ FluentBuilder2/EmailReport.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace FluentBuilder2
+{
+    public class EmailReport
+    {
+        public string Enderecamento { get; set; }
+        public string MontagemEmail { get; set; }
+
+        public override string ToString() =>
+            new StringBuilder()
+            .AppendLine(Enderecamento)
+            .AppendLine(MontagemEmail)
+            .ToString();
+    }
+}
